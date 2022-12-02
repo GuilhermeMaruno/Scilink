b728fac4e3a11439aaf361e873f03e5743da10ad
GuilhermeMaruno <83870586+GuilhermeMaruno@users.noreply.github.com>
2022-12-02 01:17:00 -0300

    Add files via upload

---
+++ Scilink/public/index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="utf-8">
+	<title>Scilink - A plataforma para ciência</title>
+
+
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Scilink - A plataforma para ciência" />
+	<meta property="og:description" content="Venha otmizar os seus projetos" />
+	<meta property="og:url" content="http://scilink.com" />
+	<meta property="og:image" content="http://scilink.com/img/og-image.png" />
+
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="shortcut icon" href="../icon.ico">
+	<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/menu.css">
+<link rel="stylesheet" href="css/style2.css">
+
+
+<link rel="stylesheet" type="text/css" media="screen" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+	<link href="https://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:400,500,700'>
+  <script> document.documentElement.classList.add("js");</script>
+</head>
+
+<body>
+
+	<header class="header">
+		<div class="container">
+			<div class="grid-2" >
+			<a href="#" >
+				<img src="img/desafio/logoheader.svg" alt="Bikcraft">
+			</a>
+		</div>
+			
+			  
+				<nav class="menu">
+					<ul>
+						<li><a href="../app/Views/telaLogin.php">Login</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+  <section class="introducao">
+    <h1 data-anime="400" class="fadeInDown"> <<< SciLink >>> <br>- A sua plataforma de ciência -</h1>
+    <div class="container">
+      <blockquote data-anime="800" class="quote-externo fadeInDown">
+        <p>Consiga agilidade ao seu projeto na maior comunidade de apoio cientifico da internet </p><br>
+        <p>Tudo desenvolvido pela equipe da <a href="" target="_blank"><span class="destaque">@scilink </span></a> em conjunto com cientistas de todas as áreas </p>
+      </blockquote>
+      <div class="call">
+        <h4 data-anime="1200"> É um cientista em busca de ajuda? </h4>
+        <a data-anime="1600" href="../app/Views/telaCadastro.php" class="btn-preto"><<< Cadastre-se >>></a>
+      </div>
+    </div>
+  </section>
+
+  <section class="provas-antigas">
+    <div class="container">
+      <h2 class="subtitulo" data-anime="2000">Cientistas</h2>
+      <ul class="fotos_lista">
+        <li class="grid-8"> <img src="img/desafio/cientista1.svg" alt="Provas Antigas"></li>
+        <li class="grid-8">
+          <div class="texto">
+            <h3>Aumente a produtividade <br>dos seus projetos</h3>
+            <p>Consulte os cientistas cadastrados e encontre aqueles com o seu perfil de trabalho para que possam se ajudar mutualmente.</p>
+          </div>
+
+        </li>
+      </ul>
+    </div>
+  </section>
+
+  <section class="curso container">
+    <h2 class="subtitulo">Projetos</h2>
+    <ul class="fotos_lista">
+      <li class="grid-8">
+        <div class="texto mobile">
+          <h3>Escolha o projeto com a <br>sua cara</h3>
+          <p>Conheça todos os projetos a fundo, filtre pelas áreas e escolha aqueles que mais te despertarem interesse e participe com sua colaboração.</p>
+        </div>
+       
+      </li>
+      <div class="absolute">
+        <li class="grid-8"> <img src="img/desafio/cientista2.svg" alt="Cursos Etec"></li>
+      </div>
+    </ul>
+  </section>
+
+
+  <section class="checklist">
+    <div class="container">
+      <br><br><br><br>
+      <img class="ideacao" src="img/desafio/cientista3.svg" alt="checklist">
+    </div>
+    <div class="call">
+      
+    </div>
+  </section>
+
+
+  <footer>
+		<div class="footer">
+			<div class="container">
+
+				<div class="grid-3 footer_historia">
+					<label>SciLink</label>
+				</div>
+
+
+			</div>
+		</div>
+
+		<div class="copy">
+			<div class="container">
+				<p class="grid-16">Scilink 2022 - todos os direitos reservados</p>
+			</div>
+		</div>
+	</footer>
+
+  	<script src="js/foto.js"></script>
+	<script src="js/simple-anime.js"></script>
+	<script src="js/simple-form.js"></script>
+	<script src="js/script.js"></script>
+	<script src="js/dropdown.js"></script>
+	<!-- JavaScript -->
+			
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-1VDDWMRSTH"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	  gtag('config', 'G-1VDDWMRSTH');
+	</script><script>
+	try {
+	  fetch(new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", { method: 'HEAD', mode: 'no-cors' })).then(function(response) {
+		return true;
+	  }).catch(function(e) {
+		var carbonScript = document.createElement("script");
+		carbonScript.src = "//cdn.carbonads.com/carbon.js?serve=CK7DKKQU&placement=wwwjqueryscriptnet";
+		carbonScript.id = "_carbonads_js";
+		document.getElementById("carbon-block").appendChild(carbonScript);
+	  });
+	} catch (error) {
+	  console.log(error);
+	}
+	</script>
+
+</body>
+
+</html>
---
+++ Scilink/public/js/dinput.js
@@ -0,0 +1,14 @@
+function toggleFormElements(bDisabled) { 
+    var inputs = document.getElementsByTagName("input"); 
+    for (var i = 0; i < inputs.length; i++) { 
+        inputs[i].disabled = bDisabled;
+    } 
+    var selects = document.getElementsByTagName("select");
+    for (var i = 0; i < selects.length; i++) {
+        selects[i].disabled = bDisabled;
+    }
+    var textareas = document.getElementsByTagName("textarea"); 
+    for (var i = 0; i < textareas.length; i++) { 
+        textareas[i].disabled = bDisabled;
+    }
+}
